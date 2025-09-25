b07247a5706ecfa0bb44367791cab84d07f13652
Fiaca22 <tuemail@ejemplo.com>
2025-09-25 17:16:25 -0300

    segunda subida clase 28/8

---
+++ testclase288.py
@@ -0,0 +1,10 @@
+lista = [1, 2, 3, 4, 5, 6]
+aux = lista
+print ("aux:", aux)
+aux.pop(0)
+aux.pop(1)
+print ("aux post cambios:", aux)
+print ("lista post cambios:", lista)
+# Al modificar 'aux', también se modifica 'lista' 
+# porque ambas variables apuntan a la 
+# misma lista en memoria.
